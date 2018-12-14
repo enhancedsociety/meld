14abcc9c00946cf3117d78adcf45538e32134702
troowala <tan@enhancedsociety.com>
2018-12-14 14:17:17 +1000

    "Simplify Compliance"

--- contracts/GoldSimple.sol
+++ contracts/GoldSimple.sol
@@ -5,61 +5,14 @@ import "localhost/zeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 import "localhost/zeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 import "localhost/zeppelin/contracts/token/ERC20/ERC20Mintable.sol";
 import "localhost/zeppelin/contracts/ownership/Ownable.sol";
+import "./SimpleCompliance.sol";
 
-contract GOLDSimple is ERC20Detailed, ERC20Burnable, ERC20Mintable, Ownable
+contract GOLDSimple is ERC20Detailed, ERC20Burnable, ERC20Mintable, Ownable, SimpleCompliance
     {
         using SafeMath for uint256;
 
-        bool internal _WhitelistOnly;
-
         constructor (string name, string symbol, uint8 decimals) public ERC20Detailed(name, symbol, decimals) {
-        	_WhitelistOnly = false;
         }
-
-        // Whitelist Accounts
-	    mapping (address => bool) public whitelistAccount;
-	    event Whitelisted(address target, bool whitelisted);
-	    function whitelistAccount(address target, bool whitelisted) onlyOwner public {
-	        whitelistAccount[target] = whitelisted;
-	        emit Whitelisted(target, whitelisted);
-	    }
-
-	    // Blacklist Accounts
-	    mapping (address => bool) public blacklistAccount;
-	    event Blacklisted(address target, bool blacklisted);
-	    function blacklistAccount(address target, bool blacklisted) onlyOwner public {
-	        blacklistAccount[target] = blacklisted;
-	        emit Blacklisted(target, blacklisted);
-	    }
-
-
-	    // Toggle WhitelistOnly
-	    function toggleWhitelist() public onlyOwner returns (bool) {
-	        _WhitelistOnly = !_WhitelistOnly;
-	        return _WhitelistOnly;
-	    }
-
-	    // WhitelistOnly Status
-	    function WhitelistOn() public view returns (bool) {
-	        return _WhitelistOnly;
-	    }
-
-	    // Ensure Sender is compliant
-	    modifier onlycompliant() {
-	    require(!blacklistAccount[msg.sender]);
-	    if (_WhitelistOnly) {
-            require(whitelistAccount[msg.sender]);
-        	}
-	    _;
-	    }
-
-	    // CheckCompliance
-	    function checkCompliance(address toCheck) internal view {
-        require(!blacklistAccount[toCheck]);
-        if (_WhitelistOnly) {
-        require(whitelistAccount[toCheck]);
-        }
-	    }
 
 	    // transfer
 	    function transfer(address to, uint256 value) public onlycompliant returns (bool) {
